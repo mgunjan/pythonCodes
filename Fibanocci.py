4af3ae00253740aebb82c58a2ef7a7c33969c010
mgunjan <milind.gunjan@gmail.com>
2015-04-26 00:48:32 -0400

    Fibonacci

---
+++ Fibanocci.py
@@ -0,0 +1,19 @@
+def rec_fibonacci(n):
+	
+	if n < 0:
+		return None
+	elif n == 0:
+		return 0
+	elif n == 1:
+		return 1
+	else:
+		return rec_fibonacci(n-1) + rec_fibonacci(n-2) 
+
+
+res = rec_fibonacci(10)
+print(res)
+
+
+
+
+
